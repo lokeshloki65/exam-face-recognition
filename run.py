10a5e6e1d6032549aefd762c97fa0c9d0ee46bac
Lokesh <lokesh152005@gmail.com>
2025-04-19 02:44:31 +0530

    run.py

--- run.py
+++ run.py
@@ -1,26 +1,15 @@
+from flask import Flask, render_template
 import os
-import audio
-import head_pose
-import detection
-import graph
-import threading as th
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Import audio only if not on Render
+if not os.environ.get("RENDER"):
+    import audio
 
 if __name__ == "__main__":
-    head_pose_thread = th.Thread(target=head_pose.pose)
-    audio_thread = th.Thread(target=audio.sound)
-    detection_thread = th.Thread(target=detection.run_detection)
-    graph_thread = th.Thread(target=graph.run_graph)
-
-    head_pose_thread.start()
-    audio_thread.start()
-    detection_thread.start()
-    graph_thread.start()
-
-    head_pose_thread.join()
-    audio_thread.join()
-    detection_thread.join()
-    graph_thread.join()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=10000)
